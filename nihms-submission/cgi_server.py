bd4d912a44706d97db4681693f9447ceeaf48fbd
Elliot Metsger <emetsger@jhu.edu>
2017-12-12 10:40:53 -0500

    Update HTTP CGI gateway with https://github.com/DataConservancy/pass-http-cgi/pull/2

--- nihms-submission/cgi_server.py
+++ nihms-submission/cgi_server.py
@@ -8,23 +8,17 @@ import BaseHTTPServer
 from subprocess import call
 
 # Edit these values to control the server behavior
-PORT    = os.environ['PY_CGI_PORT']
-DEBUG_PORT = os.environ['FTP_SUBMISSION_DEBUG_PORT']
+PORT = int(os.getenv('PY_CGI_PORT', "8080"))
+DEBUG_PORT = os.getenv('FTP_SUBMISSION_DEBUG_PORT', "5005")
 FTP_CONFIGURATION_KEY = os.getenv('FTP_CONFIGURATION_KEY', "local")
-COMMAND = "java -jar /usr/local/src/nihms-submission/nihms-cli/target/nihms-cli-1.0.0-SNAPSHOT-shaded.jar"
-ARGS    = "/usr/local/src/nihms-submission/nihms-cli/src/main/resources/FilesystemModelBuilderTest.properties " + FTP_CONFIGURATION_KEY
+COMMAND = "java"
+ARGS    = ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=" + DEBUG_PORT,
+           "-jar",
+           "/usr/local/src/nihms-submission/nihms-cli/target/nihms-cli-1.0.0-SNAPSHOT-shaded.jar",
+           "/usr/local/src/nihms-submission/nihms-cli/src/main/resources/FilesystemModelBuilderTest.properties",
+           FTP_CONFIGURATION_KEY
+          ]
 
-try:
-    os.environ['PY_CGI_PORT']
-except KeyError:
-    print "Please set the environment variable PY_CGI_PORT"
-    sys.exit(1)
-
-try:
-    os.environ['FTP_SUBMISSION_DEBUG_PORT']
-except KeyError:
-    print "Please set the environment variable FTP_SUBMISSION_DEBUG_PORT"
-    sys.exit(1)
 
 # Define a request handler class that runs a command when it receives a POST
 class RequestHandler (BaseHTTPServer.BaseHTTPRequestHandler):
@@ -34,21 +28,28 @@ class RequestHandler (BaseHTTPServer.BaseHTTPRequestHandler):
         # Execute the command and prepare the POST response code.
         # Prints an error message to the console on failure.
         # This is a blocking function.  Use subprocess.Popen for non-blocking.
-        status = call(["java", "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=" + DEBUG_PORT, "-jar", "/usr/local/src/nihms-submission/nihms-cli/target/nihms-cli-1.0.0-SNAPSHOT-shaded.jar", "/usr/local/src/nihms-submission/nihms-cli/src/main/resources/FilesystemModelBuilderTest.properties", FTP_CONFIGURATION_KEY])
-        if status != 0:
-            print "Command '{} {}' failed with code {}".format(COMMAND, ARGS, status)
+        try:
+            status = call([COMMAND]+list(ARGS))
+            if status != 0:
+                args = " ".join(str(x) for x in ARGS)
+                print "Command '{} {}' failed with code {}".format(COMMAND, args, status)
+                code = 500
+            else:
+                code = 200
+        except:
+            args = " ".join(str(x) for x in ARGS)
+            print "Command '{} {}' failed with an exception".format(COMMAND, args)
             code = 500
-        else:
-            code = 200
 
         # Prepare and send the response header
         s.send_response(code)
         s.send_header("Content-type", "text/html")
+        s.send_header("Access-Control-Allow-Origin", "*")
         s.end_headers()
 
 # Create and run the server
 if __name__ == "__main__":
-    httpd = BaseHTTPServer.HTTPServer(("", int(PORT)), RequestHandler)
+    httpd = BaseHTTPServer.HTTPServer(("", PORT), RequestHandler)
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
