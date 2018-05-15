3f7bfd6020b2accc0fd9de70e8efa74e523de48d
Aaron Birkland <apb@jhu.edu>
2018-05-15 14:56:15 -0400

    Remove bootstrap container, use mounted assets instead

--- fcrepo/4.7.5-SNAPSHOT/bin/entrypoint.sh
+++ fcrepo/4.7.5-SNAPSHOT/bin/entrypoint.sh
@@ -47,7 +47,8 @@ OPTS="-Dfcrepo.home=${FCREPO_HOME}
       -Dactivemq.broker.uri=${ACTIVEMQ_BROKER_URI}                           \
       -Dfcrepo.dynamic.jms.port=${FCREPO_JMS_PORT}                           \
       -Dfcrepo.dynamic.stomp.port=${FCREPO_STOMP_PORT}                       \
-      -Dcom.arjuna.ats.arjuna.objectstore.objectStoreDir=${ARJUNA_OBJECTSTORE_DIRECTORY}"
+      -Dcom.arjuna.ats.arjuna.objectstore.objectStoreDir=${ARJUNA_OBJECTSTORE_DIRECTORY} \
+      -Dfcrepo.properties.management=${FCREPO_PROPERTIES_MANAGEMENT}"
 
 if [ -n "${FCREPO_JMX_PORT}" ]
 then
