0f960f58db8ca9b9e6ad313a56ae8c2e4a7c0ba4
derekbelrose <derekbelrose@users.noreply.github.com>
2018-06-25 15:49:35 -0400

    Fixed a typo in the AWS entrypoint. (#93)
    I fixed a typo that was causing problems in AWS.
    Built 0.0.6-2.2-2 and pinned to sha256:f850cb53b973c8ac1e1e1fe45e06ab9adaf31aa6b4f801d4a19b01ae86eeaf9f

--- deposit-services/0.0.6-2.2/bin/aws_entrypoint.sh
+++ deposit-services/0.0.6-2.2/bin/aws_entrypoint.sh
@@ -2,10 +2,11 @@
 
 aws s3 cp s3://pass-configuration-files/entrypoints/depositservices.sh /bin/depositservices_entrypoint.sh
 
-if [ -f /bin/depositservices_entrypoints.sh ]; then
-    echo "depositservices.sh found...running"
+if [ -f /bin/depositservices_entrypoint.sh ]; then
+    echo "depositservices_entrypoint.sh found...running"
     chmod 700 /bin/depositservices_entrypoint.sh
     /bin/depositservices_entrypoint.sh
 else
     java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -jar deposit-messaging.jar listen
 fi
+
