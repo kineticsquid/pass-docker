5a64d4dc5fc1b20524c5c77446f6620f92d7f8af
Tim Martin <timmco@jhu.edu>
2022-11-09 08:25:46 -0700

    add support for docker-compose to docker-removeall.sh

--- docker-removeall.sh
+++ docker-removeall.sh
@@ -8,8 +8,22 @@
 MAX_WAIT=45  # Max time to wait for compose project to exit
 EACH_WAIT=5  # Sleep time between iterations of waiting for compose project to exit
 
-DOCKER="/usr/bin/env docker"
+ENV=/usr/bin/env
+DOCKER="$ENV docker"
 TIMEOUT_MINIMAL=1
+
+# Currently "Docker Desktop" bundles compose as part of docker.
+# Earlier distributions, including those on servers, would likely require
+# that docker-compose be installed.  Find which we have... and if not found
+# then we will avoid any compose-based cleanup but still prune docker, etc.
+COMPOSE=""
+for i in 'docker compose' docker-compose ; do
+  ver=$( $i version 2> /dev/null )
+  if [ 0 -eq $? ] && [[ "$ver" =~ ^Docker\ Compose\ version\ v ]] ; then
+    COMPOSE="$i"
+    break
+  fi
+done
 
 function shutdownCompose() {
   # Arguments:
@@ -59,9 +73,12 @@ function dockerInventory() {
   #   Output a record of Docker compose projects, Docker containers, images, etc.
   local seperator="---------"
   printf "\n\n==== Current Docker Inventory ====\n"
-  printf "\nDocker compose projects:\n"
-  $DOCKER compose ls
-  printf "%s\n" $seperator
+
+  if [ -n "$COMPOSE" ] ; then
+    printf "\nDocker compose projects:\n"
+    $COMPOSE ls
+    printf "%s\n" $seperator
+  fi
 
   printf "\nContainers:\n"
   $DOCKER ps --all
@@ -82,7 +99,7 @@ function dockerRemoveComposeProjects() {
   #     kill
   #     rm
   local composeProject
-  local composeProjects=$( $DOCKER compose ls --all --format json | jq -r .[].Name )
+  local composeProjects=$( $COMPOSE ls --all --format json | jq -r .[].Name )
   if [ -z "$composeProjects" ] ; then
     printf "No docker compose projects found\n"
   else
@@ -90,11 +107,11 @@ function dockerRemoveComposeProjects() {
 
       for action in stop kill ; do
         printf "Action: '$action' on docker compose service: '%s'\n" $composeProject
-        $DOCKER compose $action  $composeProject  --timeout=$TIMEOUT_MINIMAL
+        $COMPOSE $action  $composeProject  --timeout=$TIMEOUT_MINIMAL
       done
 
       printf "Removing docker compose service and anonymous volumes: '%s'\n" $composeProject
-      $DOCKER compose rm --stop --volumes --force "$composeProject" --timeout=1
+      $COMPOSE rm --stop --volumes --force "$composeProject" --timeout=1
     done
   fi
   printf "  done with docker compose project cleanup\n"
@@ -127,8 +144,16 @@ function dockerPrune() {
 date
 dockerInventory
 
-dockerRemoveComposeProjects
-shutdownCompose
+if [ -n "$COMPOSE" ] ; then
+  printf "Using COMPOSE='$COMPOSE'\n"
+  dockerRemoveComposeProjects
+  shutdownCompose
+else
+  printf "Could not identify docker-compose (or docker with compose)...\n"
+  printf "  skipping docker-compose cleanup bits!\n\n"
+  exit -1
+fi
+
 dockerRemoveContainers
 dockerPrune
 
