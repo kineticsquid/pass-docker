e72bb52b7500675675ff069a544221182145d2d1
John Abrahams <jabrah20@jhu.edu>
2022-12-15 14:08:38 -0600

    Force tests to wait for docker startup (#301)
    * Force tests to wait for docker startup
    
    * Fix file path typo
    
    * Try upping max retries
    
    * Fix typo in hosts setting. Pull back polling retries

---
+++ wait-to-start.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+
+PI_FEDORA_EXTERNAL_BASE="http://pass.local:8080/fcrepo/rest"
+PI_FEDORA_USER="fedoraAdmin"
+PI_FEDORA_PASS="moo"
+PI_MAX_ATTEMPTS=100
+
+# curl \
+#     -I \
+#     -u ${PI_FEDORA_USER}:${PI_FEDORA_PASS} \
+#     --write-out ${http_code} \
+#     --silent \
+#     -o /dev/stderr \
+#     --retry 50 \
+#     --retry-delay 5 \
+#     --retry-max-time 300 \
+#     --max-time 10 \
+#     ${PI_FEDORA_EXTERNAL_BASE}
+
+CMD="curl -I -u ${PI_FEDORA_USER}:${PI_FEDORA_PASS} --write-out %{http_code} --silent -o /dev/stderr ${PI_FEDORA_EXTERNAL_BASE}"
+echo "Waiting for response from Fedora via 'curl -I -u <u>:<p> ${PI_FEDORA_EXTERNAL_BASE}'"
+
+RESULT=0
+max=${PI_MAX_ATTEMPTS}
+i=1
+
+until [ ${RESULT} -eq 200 ]
+do
+    sleep 5
+    
+    RESULT=$(${CMD})
+
+    if [ $i -eq $max ]
+    then
+        echo "Reached max attempts"
+        exit 1
+    fi
+
+    i=$((i+1))
+    echo "Trying again, result was ${RESULT}"
+done
+
+echo "Fedora is up."
