83ddabb3b183aa0d9f162df9e02eb328b6d7724a
John Abrahams <jabrah20@jhu.edu>
2023-01-23 13:48:44 -0500

    Remove old services

--- demo.sh
+++ demo.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Example running:
-# ./demo.sh up -d --quiet-pull --no-build --wait
+# ./demo.sh up -d --quiet-pull --no-build
 
 # .env file(s) specified in docker-compose files
-docker compose -f eclipse-pass.base.yml -f eclipse-pass.local.yml $@
+# Some images need a .env file specified here or else will throw an error
+# The base .env is sufficient for those, while the .env files specified in
+# the various compose files work at runtime
+docker compose --env-file .eclipse-pass.base_env -f eclipse-pass.base.yml -f eclipse-pass.local.yml $@
